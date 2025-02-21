16d7220e40c5ae3c6815530cc9d554d6b215227a
dzhuleikhan <dzhuleikhan@gmail.com>
2025-02-21 18:58:19 +0300

    First deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-export default {
-  server: {
-    host: true,
-    open: true,
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "",
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
   },
-};
+  server: {
+    open: true,
+    host: true,
+  },
+});
