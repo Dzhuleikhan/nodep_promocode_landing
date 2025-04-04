36e1a52b087817d7a9c4dc7e446a8cca43f24ef7
dzhuleikhan <dzhuleikhan@gmail.com>
2025-04-04 15:28:32 +0700

    Big Bass Splash done

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,6 +1,6 @@
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "GATES81";
+export const defaulPromocode = "BASS98";
 document
   .querySelector(".hero-modal-open-btn")
   .setAttribute("data-promocode", defaulPromocode);
