03800df58b4caa794dd097a898748c066257ac83
dzhuleikhan <dzhuleikhan@gmail.com>
2025-04-04 17:54:54 +0700

    Sweet Bonanza Done

--- modules/animations.js
+++ modules/animations.js
@@ -1,11 +1,46 @@
 import gsap from "gsap";
 import { Power3 } from "gsap";
 
-gsap.to(".hero-main-img", {
+gsap.to(".hero-img", {
+  scale: 1.05,
+  ease: "none",
+  yoyo: true,
+  duration: 1,
+  repeat: -1,
+});
+gsap.to(".heart", {
+  scale: 0.9,
+  ease: "none",
+  yoyo: true,
+  duration: 1,
+  repeat: -1,
+});
+gsap.to(".cloud-left", {
+  x: -25,
+  ease: "none",
+  yoyo: true,
+  duration: 2,
+  repeat: -1,
+});
+gsap.to(".cloud-right", {
+  x: 25,
+  ease: "none",
+  yoyo: true,
+  duration: 2,
+  repeat: -1,
+});
+gsap.to(".lollipop", {
+  y: -20,
+  ease: "none",
+  yoyo: true,
+  duration: 2,
+  repeat: -1,
+});
+gsap.to(".bomb", {
   y: 20,
   ease: "none",
   yoyo: true,
-  duration: 4,
+  duration: 2,
   repeat: -1,
 });
 gsap.to(".star-img", {
