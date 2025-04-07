2d87ed828a9478cfd558b7c81aadd9e67b0b377e
dzhuleikhan <dzhuleikhan@gmail.com>
2025-04-07 17:59:10 +0700

    Monkey Heist Done

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,6 +1,6 @@
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "BUFFALO65";
+export const defaulPromocode = "HEIST75";
 document
   .querySelector(".hero-modal-open-btn")
   .setAttribute("data-promocode", defaulPromocode);
