e8a86bb175e84112424fa16e916aa8c0bc5c2bf5
dzhuleikhan <dzhuleikhan@gmail.com>
2025-04-07 17:42:11 +0700

    Changed promocode

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,6 +1,6 @@
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "HOUNDS98";
+export const defaulPromocode = "BUFFALO65";
 document
   .querySelector(".hero-modal-open-btn")
   .setAttribute("data-promocode", defaulPromocode);
