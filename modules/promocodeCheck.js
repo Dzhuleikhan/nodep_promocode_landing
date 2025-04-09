a16a81df43ec8ef2368ba1b2fb6df3aa6918bf8e
dzhuleikhan <dzhuleikhan@gmail.com>
2025-04-09 16:27:33 +0700

    WEB 20 Gates for CZ

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,6 +1,6 @@
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "GGATESPL";
+export const defaulPromocode = "GGATESCZ";
 document
   .querySelector(".hero-modal-open-btn")
   .setAttribute("data-promocode", defaulPromocode);
