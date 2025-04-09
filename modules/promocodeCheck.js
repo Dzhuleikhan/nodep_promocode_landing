8eea3d77fc7143fec0425b197d20691f593dc30e
dzhuleikhan <dzhuleikhan@gmail.com>
2025-04-09 16:24:57 +0700

    WEB 20 Gates for PL

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,6 +1,6 @@
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "GATES81";
+export const defaulPromocode = "GGATESPL";
 document
   .querySelector(".hero-modal-open-btn")
   .setAttribute("data-promocode", defaulPromocode);
