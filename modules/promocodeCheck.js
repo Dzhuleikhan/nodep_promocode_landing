210c0801dce026ddc5d8165655922d87413cc718
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-02 13:22:00 +0300

    Changed promocode

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -2,7 +2,7 @@ import { newDomain } from "./fetchingDomain";
 import { formData } from "./formAuth";
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "BITFREE888";
+export const defaulPromocode = "444JOKER";
 
 export const receivedPromocode = (
   getUrlParameter("promocode") || defaulPromocode
