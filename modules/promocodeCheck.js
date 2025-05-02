3ec5fc92fa7a588fa46d7754aabb4a0fa43977c1
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-02 13:20:03 +0300

    Changed promocode

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -2,7 +2,7 @@ import { newDomain } from "./fetchingDomain";
 import { formData } from "./formAuth";
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "ROYAL7211";
+export const defaulPromocode = "ROYALFREE";
 const headerLogoLink = document.querySelector(".header-logo-link");
 
 export const receivedPromocode = (
