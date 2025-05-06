d9ee83cf3ef9fb19dcc341f51416b31b4d835e94
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-06 16:49:43 +0300

    Changed INR bonus amount

--- public/data.js
+++ public/data.js
@@ -332,8 +332,8 @@ export const countryCurrencyData = [
     countryCurrencySymbol: "₹",
     countryCurrencyFullName: "Indian Rupee",
     countryCurrencyIcon: "./img/currencies/inr.svg",
-    amount: "420.000",
-    spins: "25FS",
+    amount: "415.000",
+    spins: "200FS",
   },
   {
     countries: ["KG"],
