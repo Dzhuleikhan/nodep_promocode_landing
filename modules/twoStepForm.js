59a78da9fa51bf1fe5c30c99bfcfd9fd3b8fa646
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-28 18:41:03 +0400

    encoded email and password

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -931,7 +931,7 @@ twoStepFormMain.addEventListener("submit", (e) => {
 
   window.location.href = `https://${newDomain}/api/register?env=prod&type=email&currency=${currency}&email=${encodeURIComponent(
     email
-  )}&password=${password}&phone=${phone}&bonus=${bonus}${
+  )}&password=${encodeURIComponent(password)}&phone=${phone}&bonus=${bonus}${
     promocode ? "&promocode=" + promocode : ""
   }&lang=${lang}${firstName ? "&f_name=" + firstName : ""}${
     lastName ? "&l_name=" + lastName : ""
@@ -945,7 +945,7 @@ twoStepFormMain.addEventListener("submit", (e) => {
   console.log(
     `https://${newDomain}/api/register?env=prod&type=email&currency=${currency}&email=${encodeURIComponent(
       email
-    )}&password=${password}&phone=${phone}&bonus=${bonus}${
+    )}&password=${encodeURIComponent(password)}&phone=${phone}&bonus=${bonus}${
       promocode ? "&promocode=" + promocode : ""
     }&lang=${lang}${firstName ? "&f_name=" + firstName : ""}${
       lastName ? "&l_name=" + lastName : ""
