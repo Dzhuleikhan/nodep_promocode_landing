0bf2e2a913b80e132ca0dca0469b8f69c4c1c31a
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-29 21:53:30 +0400

    Slots Catalog 50FS Bison Storm Done

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,7 +1,7 @@
 import { newDomain } from "./fetchingDomain";
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "BONUS50BET";
+export const defaulPromocode = "CATALOG50FREE";
 const headerLogoLink = document.querySelector(".header-logo-link");
 
 export const receivedPromocode = (
