f56feec845d0bec2d24f84ba0c771ca71636696f
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-29 21:10:54 +0400

    SpicyCasinos 50FS Bison Storm Done

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,7 +1,7 @@
 import { newDomain } from "./fetchingDomain";
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "BISON98";
+export const defaulPromocode = "BONUS50BET";
 const headerLogoLink = document.querySelector(".header-logo-link");
 
 export const receivedPromocode = (
