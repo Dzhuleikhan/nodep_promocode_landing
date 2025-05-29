bf84d41cd8d6a8e37998610afc7a61de7c5f1da0
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-29 20:45:35 +0400

    polandbonusesfinder CryptoHighroller 200FS Done

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -2,7 +2,7 @@ import { newDomain } from "./fetchingDomain";
 import { formData } from "./formAuth";
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "HIGHROLLERSFINDER";
+export const defaulPromocode = "HIGHROLLERSPLFIND";
 
 export const receivedPromocode = (
   getUrlParameter("promocode") || defaulPromocode
