f81446ed23333ee4d6b5727f5cb58afdb4c9d5dc
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-30 16:55:15 +0400

    Magyar 100Fs Buffalos Sun BUF100KASZ Done

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,7 +1,7 @@
 import { newDomain } from "./fetchingDomain";
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "SLOTFREESTORM";
+export const defaulPromocode = "BUF100KASZ";
 const headerLogoLink = document.querySelector(".header-logo-link");
 
 export const receivedPromocode = (
