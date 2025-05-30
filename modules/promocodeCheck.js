b0c42b0830e4a21e631df27e66a9a7ad59015dba
dzhuleikhan <dzhuleikhan@gmail.com>
2025-05-30 15:02:29 +0400

    polandbonusesfinder 222FS Monkey Heist done

--- modules/promocodeCheck.js
+++ modules/promocodeCheck.js
@@ -1,7 +1,7 @@
 import { newDomain } from "./fetchingDomain";
 import { getUrlParameter } from "./params";
 
-export const defaulPromocode = "FINDER150FREE";
+export const defaulPromocode = "PL222SLOT";
 const headerLogoLink = document.querySelector(".header-logo-link");
 
 export const receivedPromocode = (
