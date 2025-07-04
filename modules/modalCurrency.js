8e3297130bf4ee760e2a5f2a98d7ffcc121bda57
dzhuleikhan <dzhuleikhan@gmail.com>
2025-07-04 17:34:03 +0400

    Excluded "RU", "MX", "CL", "CO", "TH", "ID" and changed lang SE to SV

--- modules/modalCurrency.js
+++ modules/modalCurrency.js
@@ -99,8 +99,10 @@ async function settingModalCurrency() {
     let locationData = await getLocation();
     let countryInput = "IN";
 
-    if (countryInput === "RU") {
-      countryInput = "US";
+    const excludedCountries = ["RU", "MX", "CL", "CO", "TH", "ID"];
+
+    if (excludedCountries.includes(countryInput)) {
+      countryInput = "IN";
     }
 
     const currencyAbbr = getCountryCurrencyABBR(countryInput);
