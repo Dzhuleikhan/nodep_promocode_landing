d2836adda40960fed2fff32d668dcb3fb040954a
dzhuleikhan <dzhuleikhan@gmail.com>
2025-07-04 16:23:55 +0400

    Removed IDR and changed lang from SE to SV

--- public/data.js
+++ public/data.js
@@ -618,7 +618,7 @@ export const countryLanguagesMap = {
   CZ: ["cs"],
   SI: ["sl"],
   GR: ["el"],
-  SE: ["se"],
+  SE: ["sv"],
   SK: ["sk"],
 };
 
@@ -645,7 +645,7 @@ export const SupportedLanguages = [
   "sl",
   "el",
   "nb",
-  "se",
+  "sv",
   "sk",
 ];
 
