6b96f1462e8d12e8b29198c8c7e3465274ea909b
dzhuleikhan <dzhuleikhan@gmail.com>
2025-08-18 20:32:25 +0400

    added DE lang

--- modules/language.js
+++ modules/language.js
@@ -67,6 +67,7 @@ function updateButtonText(lang) {
     ru: "Russian",
     es: "Spanish",
     pt: "Portuguese",
+    de: "Deutsch",
   };
   headerLangBtn.setAttribute(
     "src",
@@ -96,6 +97,7 @@ async function determineLanguage() {
     RU: "ru",
     ES: "es",
     PT: "pt",
+    DE: "de",
     // Add more country codes and their corresponding languages as needed
   };
   lang = countryLangMap[location.countryCode] || "en";
