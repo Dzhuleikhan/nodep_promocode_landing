c60ee85409f58eebeef50442d4e67fa7d655b954
dzhuleikhan <dzhuleikhan@gmail.com>
2025-08-28 17:35:54 +0400

    added 2 extra params

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -914,6 +914,8 @@ if (headerbackBtn) {
 const twoStepFormMain = document.querySelector(".two-step-form");
 
 let cid = getUrlParameter("cid");
+let partner = getUrlParameter("partner");
+let offer = getUrlParameter("offer");
 
 twoStepFormMain.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -964,7 +966,7 @@ twoStepFormMain.addEventListener("submit", (e) => {
     zipCode ? "&postal=" + zipCode : ""
   }${address ? "&address=" + encodeURIComponent(address) : ""}${
     cid ? "&cid=" + cid : ""
-  }`;
+  }${partner ? "&partner=" + partner : ""}${offer ? "&offer=" + offer : ""}`;
   console.log(
     `https://${newDomain}/api/register?env=prod&type=email&currency=${currency}&email=${encodeURIComponent(
       email
@@ -978,7 +980,9 @@ twoStepFormMain.addEventListener("submit", (e) => {
       city ? "&city=" + city : ""
     }${zipCode ? "&postal=" + zipCode : ""}${
       address ? "&address=" + encodeURIComponent(address) : ""
-    }${cid ? "&cid=" + cid : ""}`
+    }${cid ? "&cid=" + cid : ""}${partner ? "&partner=" + partner : ""}${
+      offer ? "&offer=" + offer : ""
+    }`
   );
 });
 
