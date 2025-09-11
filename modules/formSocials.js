8e497b109351166fba33c4d1b2d5facd20e2304c
dzhuleikhan <dzhuleikhan@gmail.com>
2025-09-11 14:50:25 +0400

    fixed email and phone tab changing

--- modules/formSocials.js
+++ modules/formSocials.js
@@ -150,6 +150,7 @@ formModals.forEach((modal) => {
             formTab = tab;
 
             if (tab === "email") {
+              formGroupPhone.classList.remove("not-valid");
               if (emalInput.value != "" && emalInput.value.match(emailRegEx)) {
                 formStepBtnNext.disabled = false;
               } else {
@@ -157,6 +158,8 @@ formModals.forEach((modal) => {
               }
             }
             if (tab === "phone") {
+              emalInput.value = "";
+              formGroupEmail.classList.remove("not-valid");
               if (phoneInput.value != "" && socialsIti.isValidNumber()) {
                 formStepBtnNext.disabled = false;
               } else {
