bd6bc4661e9dedfb1df9aa55a5c96d804fcdb424
dzhuleikhan <dzhuleikhan@gmail.com>
2025-09-11 15:16:31 +0400

    fixed tabs changing

--- modules/formSocials.js
+++ modules/formSocials.js
@@ -150,6 +150,10 @@ formModals.forEach((modal) => {
             formTab = tab;
 
             if (tab === "email") {
+              if (!phoneInput.value != "" && !socialsIti.isValidNumber()) {
+                formGroupPhone.classList.remove("not-valid");
+                phoneInput.value = "";
+              }
               if (emalInput.value != "" && emalInput.value.match(emailRegEx)) {
                 formStepBtnNext.disabled = false;
               } else {
@@ -157,6 +161,13 @@ formModals.forEach((modal) => {
               }
             }
             if (tab === "phone") {
+              if (
+                !emalInput.value != "" &&
+                !emalInput.value.match(emailRegEx)
+              ) {
+                formGroupEmail.classList.remove("not-valid");
+                emalInput.value = "";
+              }
               if (phoneInput.value != "" && socialsIti.isValidNumber()) {
                 formStepBtnNext.disabled = false;
               } else {
