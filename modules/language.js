48335d8b4477d0541898c3771d32ca6190022e87
dzhuleikhan <dzhuleikhan@gmail.com>
2025-11-29 16:55:39 +0400

    new API

--- modules/language.js
+++ modules/language.js
@@ -1,5 +1,5 @@
 import { translations } from "/public/translations";
-import { getLocation } from "./geoLocation";
+import { geoData } from "./geoLocation";
 import { getSupportedLanguage } from "./geoLocation";
 import { settingInitialBonusValue, twoStepFormData } from "./twoStepForm";
 
@@ -86,7 +86,7 @@ function updateButtonText(lang) {
 export const availableLang = ["en", "fr"];
 
 async function determineLanguage() {
-  const location = await getLocation();
+  const location = geoData;
 
   const countryLangMap = {
     EN: "en",
