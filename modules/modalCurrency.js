fba503ce3b144e0f3723744b564e67ce076e3864
dzhuleikhan <dzhuleikhan@gmail.com>
2025-11-29 16:11:09 +0400

    new API

--- modules/modalCurrency.js
+++ modules/modalCurrency.js
@@ -1,4 +1,4 @@
-import { getLocation } from "./geoLocation";
+import { geoData } from "./geoLocation";
 import { countryCurrencyData } from "../public/data";
 import { formData } from "./formAuth";
 
@@ -54,7 +54,7 @@ function setCurrency(abbr, name, icon) {
 
 async function settingModalCurrency() {
   try {
-    let locationData = await getLocation();
+    let locationData = geoData;
     let countryInput = locationData.countryCode;
 
     const excludedCountries = ["RU", "MX", "CL", "CO", "TH", "ID"];
