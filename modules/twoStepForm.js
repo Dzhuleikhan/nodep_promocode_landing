b7791b7757f2536f3d3b5a101aaec00d29b95e1c
dzhuleikhan <dzhuleikhan@gmail.com>
2025-12-12 17:46:55 +0400

    MonkeyHeist progress 1.0

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -16,6 +16,8 @@ document.querySelectorAll("input").forEach((input) => {
   input.setAttribute("autocomplete", "off");
 });
 
+const CDN = "https://3344112-img.b-cdn.net";
+
 // ? SOCIALS TWO STEP FORM
 
 export let twoStepFormData = {
@@ -329,10 +331,16 @@ if (twoStepFormSecondStep) {
     let img = passwordShowBtn.querySelector("img");
     if (twoStepFormPasswordInput.type === "password") {
       twoStepFormPasswordInput.type = "text";
-      img.setAttribute("src", "./img/twoStepFormImg/password-hide-icon.svg");
+      img.setAttribute(
+        "src",
+        CDN + "/graphic/landings/twoStepFormImages/password-hide-icon.svg"
+      );
     } else {
       twoStepFormPasswordInput.type = "password";
-      img.setAttribute("src", "./img/twoStepFormImg/password-show-icon.svg");
+      img.setAttribute(
+        "src",
+        CDN + "/graphic/landings/twoStepFormImages/password-show-icon.svg"
+      );
     }
   });
 }
@@ -660,9 +668,11 @@ if (twoStepFormFourthStep) {
     });
     if (mathedCountry) {
       twoStepAppliedCountryInput.value = mathedCountry.name;
-      twoStepAppliedCountryImage.src = `./img/flags/${mathedCountry.slug}.svg`;
+      twoStepAppliedCountryImage.src =
+        CDN + `/graphic/flags/flag-${mathedCountry.slug}.svg`;
       twoStepAppliedCountryImage.alt = mathedCountry.name;
-      headerlogoFlag.src = `./img/flags/${mathedCountry.slug}.svg`;
+      headerlogoFlag.src =
+        CDN + `/graphic/flags/flag-${mathedCountry.slug}.svg`;
       headerlogoFlag.alt = mathedCountry.name;
       headerlogoFlag.classList.remove("hidden");
       twoStepFormData.country = mathedCountry.slug.toUpperCase();
@@ -692,7 +702,7 @@ if (twoStepFormFourthStep) {
         "pointer-events-none h-6 w-6 rounded-full overflow-hidden object-contain";
       img.width = 24;
       img.height = 24;
-      img.src = `./img/flags/${country.slug}.svg`;
+      img.src = CDN + `/graphic/flags/flag-${country.slug}.svg`;
       img.alt = country.name;
 
       const span = document.createElement("span");
