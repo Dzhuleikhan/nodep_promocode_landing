8b1abcdf6d4e10ea687c2c5628b6bab3f630d72d
dzhuleikhan <dzhuleikhan@gmail.com>
2025-12-14 17:05:30 +0400

    CIS Dog House CDN and Africa check

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "",
+  base: "https://landing-res.b-cdn.net/freespins/cis/thedoghouse/",
   esbuild: {
     supported: {
       "top-level-await": true,
