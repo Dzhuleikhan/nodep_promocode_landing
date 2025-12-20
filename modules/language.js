ba17cc849cacf50d46f8bf9d27def2386d247f34
dzhuleikhan <dzhuleikhan@gmail.com>
2025-12-20 19:53:25 +0400

    3 nre lang and removed CIO

--- modules/language.js
+++ modules/language.js
@@ -77,6 +77,9 @@ function updateButtonText(lang) {
     lt: "Lithuanian",
     hr: "Croatian",
     kz: "Kazakh",
+    fi: "Finnish",
+    dk: "Danish",
+    bg: "Bulgarian",
   };
   headerLangBtn.setAttribute(
     "src",
@@ -115,6 +118,9 @@ async function determineLanguage() {
     LT: "lt",
     HR: "hr",
     KZ: "kz",
+    DK: "dk",
+    FI: "fi",
+    BG: "bg",
     // Add more country codes and their corresponding languages as needed
   };
   lang = countryLangMap[location.countryCode] || "en";
