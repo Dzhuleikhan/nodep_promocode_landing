fe83a2acf28f5a395f7027e3480bce18d179240a
dzhuleikhan <dzhuleikhan@gmail.com>
2025-12-20 20:04:21 +0400

    3 new langs and removed CIO

--- modules/language.js
+++ modules/language.js
@@ -77,6 +77,9 @@ function updateButtonText(lang) {
     lt: "Lithuanian",
     hr: "Croatian",
     uz: "Uzbek",
+    fi: "Finnish",
+    dk: "Danish",
+    bg: "Bulgarian",
   };
   headerLangBtn.setAttribute(
     "src",
@@ -115,6 +118,9 @@ async function determineLanguage() {
     LT: "lt",
     HR: "hr",
     UZ: "uz",
+    DK: "dk",
+    FI: "fi",
+    BG: "bg",
     // Add more country codes and their corresponding languages as needed
   };
   lang = countryLangMap[location.countryCode] || "en";
