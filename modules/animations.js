f2842a7e59cbfe432972ec65a7667e68413a9f52
dzhuleikhan <dzhuleikhan@gmail.com>
2026-02-12 23:00:10 +0400

    New Ice Fishing Landing Welcome

--- modules/animations.js
+++ modules/animations.js
@@ -1,2 +1,25 @@
 import gsap from "gsap";
 import { Power3 } from "gsap";
+
+gsap.to(".left-wave", {
+  x: -20,
+  duration: 4,
+  ease: "linear",
+  repeat: -1,
+  yoyo: true,
+});
+gsap.to(".right-wave", {
+  x: 20,
+  duration: 4,
+  ease: "linear",
+  repeat: -1,
+  yoyo: true,
+});
+gsap.to(".blue-fish", {
+  y: 30,
+  rotate: -15,
+  duration: 5,
+  ease: "linear",
+  repeat: -1,
+  yoyo: true,
+});
