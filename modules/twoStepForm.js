8c11b7cf666c0d833da16605c3023a86aa5611a9
dzhuleikhan <dzhuleikhan@gmail.com>
2026-02-23 23:39:26 +0400

    new domain and one-tap google auth

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -912,6 +912,7 @@ if (headerbackBtn) {
 
 // | SUBMITTING FORM
 const twoStepFormMain = document.querySelector(".two-step-form");
+twoStepFormData.lang = localStorage.getItem("preferredLanguage");
 
 let cid = getUrlParameter("cid");
 let partner = getUrlParameter("partner");
@@ -1023,20 +1024,4 @@ if (twoStepReturnBtn) {
     twoStepDeclineInner.classList.add("hidden");
     document.body.style.overflow = "visible";
   });
-}
-
-// One-tap google auth
-window.onload = function () {
-  google.accounts.id.initialize({
-    client_id:
-      "757023558262-l0ffftmca719f5a4ksq4r5l2rugankkn.apps.googleusercontent.com",
-    callback: handleCredentialResponse,
-    auto_select: false,
-    cancel_on_tap_outside: true,
-  });
-  google.accounts.id.prompt();
-};
-
-function handleCredentialResponse() {
-  window.location.href = `https://${newDomain}/api/register?env=prod&type=google&currency=${twoStepFormData.currency}${twoStepFormData.promocode ? "&promocode=" + twoStepFormData.promocode : ""}&lang=${twoStepFormData.lang}${cid ? "&cid=" + cid : ""}${partner ? "&partner=" + partner : ""}${offer ? "&offer=" + offer : ""}`;
 }
