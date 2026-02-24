bc68b225854138e4ef701380750817f61d3727e8
dzhuleikhan <dzhuleikhan@gmail.com>
2026-02-24 22:28:02 +0400

    new domain and one-tap google auth

---
+++ modules/google-auth.js
@@ -0,0 +1,29 @@
+import { newDomain } from "./fetchingDomain";
+import { getSupportedLanguage } from "./geoLocation";
+import { getUrlParameter } from "./params";
+import { defaulPromocode, receivedPromocode } from "./promocodeCheck";
+
+// One-tap google auth
+let currencyStoredData = localStorage.getItem("currencyData");
+let currencyData = JSON.parse(currencyStoredData);
+let currency = currencyData.abbr;
+const promocode = receivedPromocode ? receivedPromocode : defaulPromocode;
+const lang = localStorage.getItem("preferredLanguage");
+const cid = getUrlParameter("cid");
+const partner = getUrlParameter("partner");
+const offer = getUrlParameter("offer");
+
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id:
+      "757023558262-l0ffftmca719f5a4ksq4r5l2rugankkn.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+    auto_select: false,
+    cancel_on_tap_outside: true,
+  });
+  google.accounts.id.prompt();
+};
+
+function handleCredentialResponse() {
+  window.location.href = `https://${newDomain}/api/register?env=prod&type=google&currency=${currency}${promocode ? "&promocode=" + promocode : ""}&lang=${lang}${cid ? "&cid=" + cid : ""}${partner ? "&partner=" + partner : ""}${offer ? "&offer=" + offer : ""}`;
+}
