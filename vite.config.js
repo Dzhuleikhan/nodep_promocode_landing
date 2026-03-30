2b59e4748ba0c5c5f824ae75a9162963965795f2
dzhuleikhan <dzhuleikhan@gmail.com>
2026-03-30 17:37:46 +0400

    base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "",
+  base: "https://landing-res.b-cdn.net/freespins/bisonstorm/50/",
   esbuild: {
     supported: {
       "top-level-await": true,
