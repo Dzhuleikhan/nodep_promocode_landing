e41b8342185bf8d70477069bc294e9b7810b98c9
dzhuleikhan <dzhuleikhan@gmail.com>
2026-03-30 15:50:17 +0400

    removed cdn

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://landing-res.b-cdn.net/freespins/bookofdead/",
+  base: "",
   esbuild: {
     supported: {
       "top-level-await": true,
