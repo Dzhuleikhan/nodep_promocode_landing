6f3828cbbb976cef3198659b5bb51b66a2b20e0e
dzhuleikhan <dzhuleikhan@gmail.com>
2026-04-04 16:26:53 +0400

    Applied new langs and RTL

--- modules/language.js
+++ modules/language.js
@@ -3,14 +3,40 @@ import { geoData } from "./geoLocation";
 import { getSupportedLanguage } from "./geoLocation";
 import { settingInitialBonusValue, twoStepFormData } from "./twoStepForm";
 import { setSpinAmount } from "./promocodeCheck";
+import { languageOptions, SupportedLanguages } from "../public/data";
 
 const CDN = "https://3344112-img.b-cdn.net";
 
 const headerLangBtn = document.querySelector(".header-lang-btn");
 const headerLangList = document.querySelector(".header-lang-list");
-const languageLinks = document.querySelectorAll(".language-link");
+const html = document.querySelector("html");
 
-let lang;
+if (headerLangList) {
+  headerLangList.innerHTML = "";
+
+  headerLangList.innerHTML = Object.entries(languageOptions)
+    .map(([langCode, { name, flag }]) => {
+      return `
+        <li>
+          <a
+            href="#"
+            data-lang="${langCode}"
+            class="language-link flex items-center gap-2 bg-[#ffffff] px-3 py-[9px] transition-all"
+          >
+            <img
+              class="pointer-events-none shrink-0 overflow-hidden rounded-full"
+              width="20"
+              height="20"
+              src="https://3344112-img.b-cdn.net/graphic/flags/flag-${flag}.svg"
+              alt="${name} flag"
+            />
+            <span class="pointer-events-none">${name}</span>
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+}
 
 if (headerLangBtn) {
   headerLangBtn.addEventListener("click", () => {
@@ -18,13 +44,20 @@ if (headerLangBtn) {
   });
 }
 
-languageLinks.forEach((link) => {
-  if (link) {
-    link.addEventListener("click", () => {
-      headerLangList.classList.remove("is-open");
-    });
-  }
-});
+function updateButtonText(lang) {
+  const langBtnImg = headerLangBtn.querySelector("img");
+  const headerLangName = document.querySelector(".header-lang-btn span");
+
+  const { name, flag } = languageOptions[lang] || languageOptions.en;
+
+  langBtnImg.setAttribute(
+    "src",
+    CDN + `/graphic/flags/flag-${flag}.svg` ||
+      CDN + `/graphic/flags/flag-en.svg`,
+  );
+  headerLangName.innerHTML = name;
+  html.setAttribute("lang", lang);
+}
 
 function updateContent(lang) {
   const elements = document.querySelectorAll("[data-translate]");
@@ -34,10 +67,51 @@ function updateContent(lang) {
   });
 }
 
+function getInitialLanguage(country, fallbackLang) {
+  const browserLang = navigator.language.split("-")[0];
+  const supportedLang = SupportedLanguages.includes(browserLang)
+    ? browserLang
+    : fallbackLang;
+
+  if (country === "BE") {
+    if (supportedLang && browserLang !== "nl") {
+      return browserLang;
+    }
+    return "en";
+  }
+  if (country === "CH") {
+    return supportedLang ?? "de";
+  }
+  if (country === "CA") {
+    return supportedLang ?? "en";
+  }
+  if (country === "CY") {
+    return supportedLang ?? "el";
+  }
+  if (country === "LU") {
+    return supportedLang ?? "fr";
+  }
+  if (country === "EE") {
+    return supportedLang ?? "et";
+  }
+
+  return fallbackLang;
+}
+
+const RTL_LANGUAGES = ["ar"];
+
 function changeLanguage(lang) {
   updateContent(lang);
   updateButtonText(lang);
   setActiveLanguageBtn(lang);
+
+  if (RTL_LANGUAGES.includes(lang)) {
+    html.setAttribute("dir", "rtl");
+    document.body.classList.add("is-rtl");
+  } else {
+    html.setAttribute("dir", "ltr");
+    document.body.classList.remove("is-rtl");
+  }
 }
 
 function setActiveLanguageBtn(currentLang) {
@@ -50,118 +124,37 @@ function setActiveLanguageBtn(currentLang) {
   });
 }
 
-function updateButtonText(lang) {
-  const headerLangBtn = document.querySelector(".header-lang-btn img");
-  const headerLangName = document.querySelector(".header-lang-btn span");
-
-  const languageNames = {
-    en: "English",
-    fr: "French",
-    ro: "Romainan",
-    hu: "Hungarian",
-    pl: "Polish",
-    cz: "Czech",
-    si: "Slovenian",
-    gr: "Greek",
-    no: "Norwegian",
-    se: "Swedish",
-    sk: "Slovak",
-    ru: "Russian",
-    es: "Spanish",
-    pt: "Portuguese",
-    de: "Deutsch",
-    az: "Azerbaijani",
-    it: "Italian",
-    ee: "Estonian",
-    lv: "Latvian",
-    lt: "Lithuanian",
-    hr: "Croatian",
-    fi: "Finnish",
-    dk: "Danish",
-    bg: "Bulgarian",
-  };
-  headerLangBtn.setAttribute(
-    "src",
-    CDN + `/graphic/flags/flag-${lang}.svg` ||
-      CDN + `/graphic/flags/flag-en.svg`
-  );
-  headerLangName.innerHTML = languageNames[lang];
-  document.querySelector("html").setAttribute("lang", lang);
-}
-
-export const availableLang = ["en", "fr"];
-
-async function determineLanguage() {
-  const location = geoData;
-
-  const countryLangMap = {
-    EN: "en",
-    FR: "fr",
-    RO: "ro",
-    HU: "hu",
-    PL: "pl",
-    CZ: "cz",
-    SI: "si",
-    GR: "gr",
-    NO: "no",
-    SE: "se",
-    SK: "sk",
-    RU: "ru",
-    ES: "es",
-    PT: "pt",
-    DE: "de",
-    AZ: "az",
-    IT: "it",
-    EE: "ee",
-    LV: "lv",
-    LT: "lt",
-    HR: "hr",
-    DK: "dk",
-    FI: "fi",
-    BG: "bg",
-    // Add more country codes and their corresponding languages as needed
-  };
-  lang = countryLangMap[location.countryCode] || "en";
-
-  return lang;
-}
-
-async function mainFunction() {
-  try {
-    lang = await determineLanguage();
-    changeLanguage(lang);
-    localStorage.setItem(
-      "preferredLanguage",
-      getSupportedLanguage(lang.toUpperCase())
-    );
-    setTimeout(() => {
-      const currencyData = JSON.parse(localStorage.getItem("currencyData"));
-      document.querySelectorAll(".current-domain").forEach((domain) => {
-        domain.innerHTML = window.location.hostname;
-      });
-      setSpinAmount();
-    }, 200);
-  } catch (error) {
-    console.error("Error determining language:", error);
-  }
-}
-mainFunction();
-
-document.querySelectorAll(".language-link").forEach((langBtn) => {
-  langBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const targetLang = e.target.getAttribute("data-lang");
-    changeLanguage(targetLang);
-    localStorage.setItem(
-      "preferredLanguage",
-      getSupportedLanguage(targetLang.toUpperCase())
-    );
-    const currencyData = JSON.parse(localStorage.getItem("currencyData"));
-    settingInitialBonusValue(currencyData.abbr);
-    twoStepFormData.lang = localStorage.getItem("preferredLanguage");
-    document.querySelectorAll(".current-domain").forEach((domain) => {
-      domain.innerHTML = window.location.hostname;
-    });
-    setSpinAmount();
+function updateCurrentDomain() {
+  document.querySelectorAll(".current-domain").forEach((domain) => {
+    domain.innerHTML = window.location.hostname;
   });
+}
+
+async function initLanguage() {
+  const fallbackLang = getSupportedLanguage(geoData.countryCode);
+  const initialLang = getInitialLanguage(geoData.countryCode, fallbackLang);
+  changeLanguage(initialLang);
+  localStorage.setItem("preferredLanguage", initialLang);
+
+  setTimeout(() => {
+    updateCurrentDomain();
+    setSpinAmount();
+  }, 200);
+}
+initLanguage();
+
+headerLangList.addEventListener("click", (e) => {
+  e.preventDefault();
+  const link = e.target.closest("a[data-lang]");
+  if (!link) return;
+  const targetLang = link.getAttribute("data-lang");
+  changeLanguage(targetLang);
+  localStorage.setItem("preferredLanguage", getSupportedLanguage(targetLang));
+
+  const currencyData = JSON.parse(localStorage.getItem("currencyData"));
+  settingInitialBonusValue(currencyData.abbr);
+  twoStepFormData.lang = localStorage.getItem("preferredLanguage");
+  updateCurrentDomain();
+  setSpinAmount();
+  headerLangList.classList.remove("is-open");
 });
