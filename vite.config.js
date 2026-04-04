3264f3ba32e3b88396e9c14489ff2dfff2aea448
dzhuleikhan <dzhuleikhan@gmail.com>
2026-04-04 17:13:33 +0400

    New langs and RTL

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "",
+  base: "https://landing-res.b-cdn.net/freespins/monkeyheist/",
   esbuild: {
     supported: {
       "top-level-await": true,
