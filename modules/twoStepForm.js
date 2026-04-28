d37266d682dcf41e383bfc0c8ac36f366b6a788d
dzhuleikhan <dzhuleikhan@gmail.com>
2026-04-28 18:52:12 +0400

    Disposable emails validation

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -6,6 +6,7 @@ import { getUrlParameter } from "./params";
 import gsap from "gsap";
 import { isValidPhoneNumber } from "libphonenumber-js";
 import { canadaProvincesCities, australiaStatesCities } from "../public/data";
+import { isDisposableEmail } from "./disposableEmail";
 import flatpickr from "flatpickr";
 import {
   defaulPromocode,
@@ -277,7 +278,8 @@ if (twoStepFormSecondStep) {
     const emailValue = twoStepFormEmailInput.value.trim();
     const passwordValue = twoStepFormPasswordInput.value.trim();
 
-    const isEmailValid = regex.test(emailValue);
+    const isEmailValid =
+      regex.test(emailValue) && !isDisposableEmail(emailValue);
     const isPasswordValid = passwordValue.length >= 6;
 
     twoStepFormEmailInput.style.color = isEmailValid
