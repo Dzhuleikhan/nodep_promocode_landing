5c1862da4fa795c8a66d59c7a9797055e55f162a
dzhuleikhan <dzhuleikhan@gmail.com>
2026-07-23 03:06:11 +0400

    new wager badge

--- modules/animations.js
+++ modules/animations.js
@@ -29,3 +29,20 @@ gsap.to(".orange-img", {
   duration: 3,
   repeat: -1,
 });
+
+gsap
+  .matchMedia()
+  .add(
+    { isDesktop: "(min-width: 993px)", isMobile: "(max-width: 992px)" },
+    (context) => {
+      gsap.to(".wager-badge", {
+        scale: 1.05,
+        rotation: context.conditions.isDesktop ? 5 : 0,
+        force3D: true,
+        ease: "none",
+        yoyo: true,
+        duration: 2,
+        repeat: -1,
+      });
+    },
+  );
