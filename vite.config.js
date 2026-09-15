9080a13fee76c7ef561ca04cbc6f3b4c2432d09a
dzhuleikhan <dzhuleikhan@gmail.com>
2026-09-15 20:06:46 +0400

    merged test branch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 const DEV_PROXY_TARGET = "https://goldbet.fun";
 
 export default defineConfig({
-  base: "https://landing-res.b-cdn.net/freespins/gatesofolympus/geo-address-test/",
+  base: "https://landing-res.b-cdn.net/freespins/gatesofolympus/",
   esbuild: {
     supported: {
       "top-level-await": true,
